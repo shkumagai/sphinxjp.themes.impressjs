4a7198a4b629e166e601e1c7389a6739b4fea045
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2012-01-18 01:54:50 +0900

    Add docs of project.

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# -- General configuration -----------------------------------------------------
+
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = u'sphinx theme for html presentation style'
+copyright = u'2012, Sphinx-users.jp'
+
+version = '0.1.0'
+
+# -- Options for HTML output ---------------------------------------------------
+
+extensions = ['sphinxjp.themecore']
+html_theme = 'impressjs'
