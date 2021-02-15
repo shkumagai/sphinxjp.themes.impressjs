73a7602b2434924521a7f7e3bfc658d23c879458
shkumagai <take.this.2.your.grave@gmail.com>
2021-02-15 20:49:48 +0900

    reformat by black

--- docs/conf.py
+++ docs/conf.py
@@ -2,17 +2,19 @@
 #
 # -- General configuration -------------------------------------
 
-source_suffix = '.rst'
-master_doc = 'index'
+source_suffix = ".rst"
+master_doc = "index"
 
-project = u'Sphinx theme for dynamic html presentation style'
-copyright = u'2012-2021, Sphinx-users.jp'
+project = u"Sphinx theme for dynamic html presentation style"
+copyright = u"2012-2021, Sphinx-users.jp"
 
-version = '0.5.0'
+version = "0.5.0"
 
 # -- Options for HTML output -----------------------------------
 
-extensions = ['sphinxjp.themes.impressjs', ]
+extensions = [
+    "sphinxjp.themes.impressjs",
+]
 
-html_theme = 'impressjs'
+html_theme = "impressjs"
 html_use_index = False
--- src/sphinxjp/themes/impressjs/directives.py
+++ src/sphinxjp/themes/impressjs/directives.py
@@ -13,7 +13,7 @@ from docutils import nodes
 from docutils.parsers.rst import Directive, directives
 from docutils.parsers.rst.roles import set_classes
 
-__docformat__ = 'reStructuredText'
+__docformat__ = "reStructuredText"
 
 
 class impressjs(nodes.General, nodes.Element):
@@ -24,22 +24,23 @@ class Impressjs(Directive):
     """
     A impressjs entry, control impressjs slide effects, actions and styles.
     """
+
     has_content = True
     required_arguments = 1
     optional_arguments = 1
     final_argument_whitespace = True
 
     option_spec = {
-        'data-x': int,
-        'data-y': int,
-        'data-z': int,
-        'data-rotate-x': int,
-        'data-rotate-y': int,
-        'data-rotate': int,
-        'data-scale-x': int,
-        'data-scale-y': int,
-        'data-scale': int,
-        'class': directives.class_option,
+        "data-x": int,
+        "data-y": int,
+        "data-z": int,
+        "data-rotate-x": int,
+        "data-rotate-y": int,
+        "data-rotate": int,
+        "data-scale-x": int,
+        "data-scale-y": int,
+        "data-scale": int,
+        "class": directives.class_option,
     }
 
     node_class = impressjs
@@ -48,51 +49,50 @@ class Impressjs(Directive):
         """ build impressjs node """
         set_classes(self.options)
         self.assert_has_content()
-        text = '\n'.join(self.content)
+        text = "\n".join(self.content)
         node = self.node_class(text, **self.options)
         self.add_name(node)
         self.state.nested_parse(self.content, self.content_offset, node)
 
         if self.arguments[0]:
-            node['ids'] += [self.arguments[0]]
-        if 'class' in self.options:
-            node['classes'].append(self.options['class'])
+            node["ids"] += [self.arguments[0]]
+        if "class" in self.options:
+            node["classes"].append(self.options["class"])
 
         return [node]
 
 
 def visit_impressjs(self, node):
     """ build div start tag for impres.js """
-    atts = {'class': 'step'}
+    atts = {"class": "step"}
 
-    if 'data-x' in node:
-        atts['data-x'] = node['data-x']
-    if 'data-y' in node:
-        atts['data-y'] = node['data-y']
-    if 'data-z' in node:
-        atts['data-z'] = node['data-z']
-    if 'data-rotate-x' in node:
-        atts['data-rotate-x'] = node['data-rotate-x']
-    if 'data-rotate-y' in node:
-        atts['data-rotate-y'] = node['data-rotate-y']
-    if 'data-rotate' in node:
-        atts['data-rotate'] = node['data-rotate']
-    if 'data-scale-x' in node:
-        atts['data-scale-x'] = node['data-scale-x']
-    if 'data-scale-y' in node:
-        atts['data-scale-y'] = node['data-scale-y']
-    if 'data-scale' in node:
-        atts['data-scale'] = node['data-scale']
+    if "data-x" in node:
+        atts["data-x"] = node["data-x"]
+    if "data-y" in node:
+        atts["data-y"] = node["data-y"]
+    if "data-z" in node:
+        atts["data-z"] = node["data-z"]
+    if "data-rotate-x" in node:
+        atts["data-rotate-x"] = node["data-rotate-x"]
+    if "data-rotate-y" in node:
+        atts["data-rotate-y"] = node["data-rotate-y"]
+    if "data-rotate" in node:
+        atts["data-rotate"] = node["data-rotate"]
+    if "data-scale-x" in node:
+        atts["data-scale-x"] = node["data-scale-x"]
+    if "data-scale-y" in node:
+        atts["data-scale-y"] = node["data-scale-y"]
+    if "data-scale" in node:
+        atts["data-scale"] = node["data-scale"]
 
-    self.body.append(self.starttag(node, 'div', **atts))
+    self.body.append(self.starttag(node, "div", **atts))
 
 
 def depart_impressjs(self, node=None):
     """ build div end tag """
-    self.body.append('</div>\n')
+    self.body.append("</div>\n")
 
 
 def setup(app):
-    app.add_node(impressjs,
-                 html=(visit_impressjs, depart_impressjs))
-    app.add_directive('impressjs', Impressjs)
+    app.add_node(impressjs, html=(visit_impressjs, depart_impressjs))
+    app.add_directive("impressjs", Impressjs)
